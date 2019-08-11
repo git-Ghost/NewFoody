7f532c1df9ef7b0b978b058e445ef1e9ee6c6eb1
abinash <apattajoshi@tekion.com>
2019-08-11 23:04:27 +0530

    Flushed branch

--- src/main/webapp/WEB-INF/static/js/submitEnable.js
+++
@@ -1,6 +0,0 @@
-var checkboxes = $("input[type='checkbox']"),
-    submitButt = $("input[value='Register'");
-
-checkboxes.click(function() {
-    submitButt.attr("disabled", !checkboxes.is(":checked"));
-});
